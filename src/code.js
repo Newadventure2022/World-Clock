f0683041c821f7ef1f2b712ad87a2cebc70b0dd1
Newadventure2022 <celias.efl.esl.classes@gmail.com>
2023-07-11 20:32:44 +0200

    added the CSS and JS

---
+++ src/code.js
@@ -0,0 +1,12 @@
+function showSelectedCity(event) {
+  if (event.target.value.length > 0) {
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY h:mm A");
+
+    alert(`It is ${currentTime} in ${event.target.value}`);
+  }
+}
+
+let citySelect = document.querySelector("#city");
+citySelect.addEventListener("change", showSelectedCity);
